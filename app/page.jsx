e3732394f3f257822b9e8bfb069d2f18646a0b2c
Quabmac <mccormack.jacob@gmail.com>
2024-06-05 12:22:45 +1000

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,8 @@
 import Link from "next/link"
+import { useRouter } from "next/navigation"
 
 const HomePage = () => {
+  const router = useRouter()
   return (
     <div>
       <h1 className="text-3xl">Welcome</h1>
