25adf9df50ff2818d2b5f012d3b9e7dee831380d
Quabmac <mccormack.jacob@gmail.com>
2024-06-05 10:11:19 +1000

    initial pushes

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,11 @@
+import "@/assets/styles/globals.css"
+
+export const metadata = {
+  title: "Property Pulse | Find the perfect rental",
+  description: "Find your dream home",
+  keywords: "rental, find rentals, find properties",
+}
+
 const MainLayout = ({ children }) => {
   return (
     <html lang="en">
